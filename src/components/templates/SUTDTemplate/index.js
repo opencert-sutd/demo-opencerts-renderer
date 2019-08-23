cf3652d2937a1f38961efd0212489eac0e8eda2f
opencert-sutd <52150238+opencert-sutd@users.noreply.github.com>
2019-08-23 12:58:42 +0800

    Add files via upload

--- src/components/templates/SUTDTemplate/index.js
+++ src/components/templates/SUTDTemplate/index.js
@@ -1,11 +1,16 @@
 import SUTDCert from "./certificate";
-
+import SUTDTranscript from "./transcript";
 
 const templates = [
   {
     id: "certificate",
     label: "Certificate",
     template: SUTDCert
+  },
+  {
+    id: "transcript",
+    label: "Transcript",
+    template: SUTDTranscript
   }
 ];
 
