ad06ddca5e1c8e23deae13738cf72c3275664441
opencert-sutd <52150238+opencert-sutd@users.noreply.github.com>
2020-01-25 15:18:59 +0800

    Update index.js

--- src/components/templates/SUTD5Template/index.js
+++ src/components/templates/SUTD5Template/index.js
@@ -1,27 +1,18 @@
-import PropTypes from "prop-types";
-import MultiCertificateRenderer from "template-utils/MultiCertificateRenderer";
+
+import SUTDCert from "./certificate";
 import SUTDTranscript from "./transcript";
 
 const templates = [
- 
+  {
+    id: "certificate",
+    label: "Certificate",
+    template: SUTDCert
+  },
   {
     id: "transcript",
-    label: "transcript",
+    label: "Transcript",
     template: SUTDTranscript
   }
 ];
 
-const addresses = [
-  "0x2456FC81C1342fB79D7C58A4682F031208A44d7F",
-  "0x96a7bEefb0A7fb6B9d2101B0A27a734fA97E7221"
-];
-
-const SUTD5THROW2019 = () => (
-  <MultiCertificateRenderer templates={templates} whitelist={addresses} />
-);
-
-SUTD5THROW2019.propTypes = {
-  certificate: PropTypes.object.isRequired
-};
-
-export default SUTD5THROW2019;
+export default templates;
