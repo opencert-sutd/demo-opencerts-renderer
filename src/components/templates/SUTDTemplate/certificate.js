c0a00b48f29605a50e1d8b80ed8929ad7928a8a1
opencert-sutd <52150238+opencert-sutd@users.noreply.github.com>
2021-09-01 20:54:37 +0800

    Update certificate.js

--- src/components/templates/SUTDTemplate/certificate.js
+++ src/components/templates/SUTDTemplate/certificate.js
@@ -172,9 +172,6 @@ const Template = ({ document }) => (
         <span style={GothamMedium165pt}>{document.recipient.Honors}</span>
       </div>
 	  
-      <div>
-        <SubPlan document={document} />
-      </div>
 
       <div className="row d-flex justify-content-center align-items-center">	       
 	  <span style={GothamMedium165pt}>{document.recipient.SubPlan}</span>	  
