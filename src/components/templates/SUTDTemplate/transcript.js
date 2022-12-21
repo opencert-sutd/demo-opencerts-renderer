6d027f039b14d75a04f1a4f062a164331c9d643a
opencert-sutd <52150238+opencert-sutd@users.noreply.github.com>
2022-12-21 17:35:53 +0800

    Update transcript.js

--- src/components/templates/SUTDTemplate/transcript.js
+++ src/components/templates/SUTDTemplate/transcript.js
@@ -127,10 +127,10 @@ export const SubjectGrades = ({ document }) => {
        <div className="col-auto">
           <span style={Arial15pt}>{t.courseCode}</span>
         </div>
-		<div className="col-1">&nbsp;</div>
+	<div className="col-1">&nbsp;</div>
 //Hidden and Added By Suresh to remove space between Subject code and title
-// <div className="col-auto">
-		<div className="col-1 grade">
+        <div className="col-auto">
+//	<div className="col-1 grade">
           <span style={Arial15pt}>{t.name}</span>
         </div>
 		<div className="col">&nbsp;</div>
@@ -183,7 +183,6 @@ export const SubjectGrades = ({ document }) => {
 
 
 export const RemarksFooter =({ document }) => {
-	
 	const Remarks = get(document, "additionalData.Remarks",undefined);
 	const Remarks1 = Remarks ? Remarks.replace(/\\n/g,'\n') : null;
 	return Remarks ? (
