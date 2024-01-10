d66f123d6f6139e626f74140eaa5dea5be7ccd5c
opencert-sutd <52150238+opencert-sutd@users.noreply.github.com>
2024-01-10 10:38:32 +0800

    Update certificate.js

--- src/components/templates/SUTDDuelTemplate/certificate.js
+++ src/components/templates/SUTDDuelTemplate/certificate.js
@@ -284,7 +284,7 @@ const Template = ({ document }) => (
 
     <div className="row">
       <div style={{ marginRight: "12.5rem" }}>&nbsp;</div>
-      <div style={{ marginRight: "32.5rem" }}>
+      <div style={{ marginRight: "33.25rem" }}>
         <div>
           <span style={GothamMedium10pt}>{document.additionalData.Signatorytype[1].type}</span>
         </div>
