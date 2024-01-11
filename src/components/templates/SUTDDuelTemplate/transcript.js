279339fc2a4c3c531861dcbfe11d2d8765882e29
opencert-sutd <52150238+opencert-sutd@users.noreply.github.com>
2024-01-11 15:37:38 +0800

    Update transcript.js

--- src/components/templates/SUTDDuelTemplate/transcript.js
+++ src/components/templates/SUTDDuelTemplate/transcript.js
@@ -124,7 +124,7 @@ export const SubjectGrades = ({ document }) => {
   const subjects = semesters.map((s, j) => {
     const semesterSubjects = s.grades.map((t, i) => (
       <div className="row" key={i}>
-	    <div>&nbsp;</div>
+	    <div>&nbsp;&nbsp;&nbsp;</div>
        <div>
           <span style={Arial15pt}>{t.courseCode}</span>
         </div>
